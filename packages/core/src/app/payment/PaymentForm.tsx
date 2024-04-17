89fd78328c0d574661acac0f49badb788d932407
mesajil-sphere <luis.mesajil@sphere.com.pe>
2024-04-17 16:43:30 -0500

    feat: remove fixed CulqiSubmitButton

--- packages/core/src/app/payment/PaymentForm.tsx
+++ packages/core/src/app/payment/PaymentForm.tsx
@@ -175,9 +175,6 @@ const PaymentForm: FunctionComponent<
                             />
                         ))}
                 </div>
-
-                <h3>My culqi button</h3>
-                <CulqiSubmitButton />
             </Form>
         );
     };
