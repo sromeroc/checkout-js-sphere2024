4df778da98e29d01511f0acc70233fde7af0bb1d
mesajil-sphere <luis.mesajil@sphere.com.pe>
2024-04-25 09:40:48 -0500

    feat: run culqi function on submit-click

--- packages/core/src/app/payment/paymentMethod/Culqi/CulqiSubmitButton2.tsx
+++ packages/core/src/app/payment/paymentMethod/Culqi/CulqiSubmitButton2.tsx
@@ -18,64 +18,6 @@ const CulqiSubmitButton2: React.FC = () => {
         document.body.appendChild(script);
         script.onload = () => {
             setupCheckout(checkoutData)
-            const culqi = () => {
-                if (Culqi.token) {  // ¡Objeto Token creado exitosamente!
-                    if (checkoutData) {
-                        const token = Culqi.token.id;
-                        console.log('Se ha creado un Token: ', token);
-                        // En esta línea de código, debes enviar el "Culqi.token.id"
-                        // hacia tu servidor con Ajax
-                        const data = JSON.stringify({
-                            "amount": 600,
-                            "currency_code": "PEN",
-                            "email": checkoutData.billingAddress?.email,
-                            "source_id": token,
-                            "capture": true,
-                            "description": "BigCommerce",
-                            "installments": 0,
-                            "metadata": {
-                                "dni": "09928494"
-                            },
-                            "antifraud_details": {
-                                "address": checkoutData.billingAddress?.address1,
-                                "address_city": checkoutData.billingAddress?.city,
-                                "country_code": checkoutData.billingAddress?.countryCode,
-                                "first_name": checkoutData.billingAddress?.firstName,
-                                "last_name": checkoutData.billingAddress?.lastName,
-                                "phone_number": checkoutData.billingAddress?.phone
-                            }
-                        });
-            
-                        //var XMLHttpRequest = require('xhr2');
-                        const xhr = new XMLHttpRequest();
-                        xhr.withCredentials = false;
-            
-                        xhr.addEventListener("readystatechange", function () {
-                            if (this.readyState === this.DONE) {
-                                console.log(this.responseText);
-                            }
-                        });
-            
-                        console.log('Antes del cargo');
-                        console.log('Data: ', data);
-                        console.log('Despues del cargo');
-            
-                        xhr.open("POST", "https://api.culqi.com/v2/charges");
-                        xhr.setRequestHeader("Authorization", "Bearer sk_test_kW32mQUjBB3KnfUD");
-                        xhr.setRequestHeader("content-type", "application/json");
-            
-                        xhr.send(data);
-                    }
-                    else {
-                        console.log('ERROR at culqi: CheckoutData is undefined');
-                    }
-                } else {
-            
-                    // Mostramos JSON de objeto error en la consola
-                    console.log('Error : ', Culqi.error);
-            
-                }
-            }
         };
 
         return () => {
@@ -85,6 +27,7 @@ const CulqiSubmitButton2: React.FC = () => {
 
     const handleClick = () => {
         openCulqi()
+        culqi()
     }
 
     return (
@@ -111,6 +54,65 @@ const setupCheckout = (checkoutData: Checkout | undefined) => {
     }
     else {
         console.log('ERROR at setupCheckout: CheckoutData is undefined');
+    }
+}
+
+const culqi = () => {
+    if (Culqi.token) {  // ¡Objeto Token creado exitosamente!
+        if (checkoutData) {
+            const token = Culqi.token.id;
+            console.log('Se ha creado un Token: ', token);
+            // En esta línea de código, debes enviar el "Culqi.token.id"
+            // hacia tu servidor con Ajax
+            const data = JSON.stringify({
+                "amount": 600,
+                "currency_code": "PEN",
+                "email": checkoutData.billingAddress?.email,
+                "source_id": token,
+                "capture": true,
+                "description": "BigCommerce",
+                "installments": 0,
+                "metadata": {
+                    "dni": "09928494"
+                },
+                "antifraud_details": {
+                    "address": checkoutData.billingAddress?.address1,
+                    "address_city": checkoutData.billingAddress?.city,
+                    "country_code": checkoutData.billingAddress?.countryCode,
+                    "first_name": checkoutData.billingAddress?.firstName,
+                    "last_name": checkoutData.billingAddress?.lastName,
+                    "phone_number": checkoutData.billingAddress?.phone
+                }
+            });
+
+            //var XMLHttpRequest = require('xhr2');
+            const xhr = new XMLHttpRequest();
+            xhr.withCredentials = false;
+
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === this.DONE) {
+                    console.log(this.responseText);
+                }
+            });
+
+            console.log('Antes del cargo');
+            console.log('Data: ', data);
+            console.log('Despues del cargo');
+
+            xhr.open("POST", "https://api.culqi.com/v2/charges");
+            xhr.setRequestHeader("Authorization", "Bearer sk_test_kW32mQUjBB3KnfUD");
+            xhr.setRequestHeader("content-type", "application/json");
+
+            xhr.send(data);
+        }
+        else {
+            console.log('ERROR at culqi: CheckoutData is undefined');
+        }
+    } else {
+
+        // Mostramos JSON de objeto error en la consola
+        console.log('Error : ', Culqi.error);
+
     }
 }
 
